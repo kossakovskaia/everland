2421a1bfa678c77a25c7f224194b7b567ec7cbc5
Alexandr Slobodyanin <sashaslab@yandex.ru>
2023-01-20 02:30:26 +0400

    fix accordion

--- accordion.js
+++ accordion.js
@@ -1,32 +1,18 @@
-// const accordionHeaderClickHandler = function (e) {
-//   document.querySelectorAll('.accordion__section').forEach(function (section) {
-//     section.querySelector('.accordion__body').style.maxHeight = '0px'
-//   })
-
-//   let accordionSection = e.target.closest('.accordion__section')
-
-//   let insideElHeight = accordionSection.querySelector('.accordion__body > *').clientHeight
-
-//   let buttonIcon = accordionSection.querySelector('.accordion__button-icon')
-//   buttonIcon.classList.add('accordion__button-icon_active')
-//   accordionSection.querySelector('.accordion__body').style.maxHeight = insideElHeight + 'px'
-// }
-
-// document.querySelectorAll('.accordion__button').forEach(function (section) {
-//   section.addEventListener('click', accordionHeaderClickHandler);
-
-// })
 const accordionButton = document.querySelectorAll('.accordion__button');
+const accordionSection = document.querySelectorAll('.accordion__section');
+function openedAccordion(e) {
+  let accordion = e.target.closest('.accordion__section');
 
-accordionButton.forEach(function (item) {
-  item.addEventListener('click', () => {
-    let accordionBody = document.querySelector('.accordion__body');
-    console.log(accordionBody)
-    // if (accordionBody.style.maxHeight) {
-    //   document.querySelectorAll('.accordion__body').forEach((item) => item.style.maxHeight = null)
-    // } else {
-    //   document.querySelectorAll('.accordion__body').forEach((item) => item.style.maxHeight = null)
-    //   accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px'
-    // }
-  })
+  if (accordion.classList.contains('accordion__section_opened')) {
+    accordion.classList.remove('accordion__section_opened')
+  } else {
+    accordionSection.forEach(function (item) {
+      item.classList.remove('accordion__section_opened')
+    })
+    accordion.classList.add('accordion__section_opened')
+  }
+}
+
+accordionButton.forEach(function (section) {
+  section.addEventListener('click', openedAccordion);
 })
